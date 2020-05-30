b489388a266ef7c5befb8f36d1ec4ed938309037
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-05-30 16:39:18 +0100

    DataManager class Added

--- app/src/main/java/com/example/addnotes/DataManager.kt
+++ app/src/main/java/com/example/addnotes/DataManager.kt
@@ -1,4 +1,36 @@
 package com.example.addnotes
 
 class DataManager {
+    //We need to add properties to class
+    //HashMap is a generic type they allow us to specify a specific type withing the type
+    //HashMap accepts two type parameters
+    //the first type will be used for lookups we will lookup our Changeinfo ID which is string
+    // the second parameter type is a type that will be stored, which for our changes it's ChangeInfo
+    val items = HashMap<String,ChangeInfo>()
+
+    //We need another property to hold our collection of changes we use array for this purpose.
+    val changes = ArrayList<ChangeInfo>()
+    init {
+    // this code will run whenever and instance of the Datamanager is created
+        intializeChanges()
+        // initialise block accepts no parameter because there no way of running them explicitly
+        // they are run automatically
+    }
+
+    // We need to initialise our Change Info class we do this by creating a function with no parameters
+    // We make this method private to the DataManager class so it's not available from anywhere else
+    private fun intializeChanges(){
+        //for the HashMap lookup we do that by using it set method we pass the Id as first parameter
+        // and the change var as the second parameter
+        var change = ChangeInfo("android_intents","Android Programming with Intents")
+        items.set(change.changeId,change)
+        //Adding a few more items ot the list
+        // using the Name specific Parameters the order of parameters in which they are used doesn't matter
+        change = ChangeInfo(title = "Java Fundmentals: The Java Language",changeId = "java_lang")
+        //using positional parameters
+        change = ChangeInfo("report_c365","Notts Waste Audit Dashboard report")
+        change = ChangeInfo("report_c365","Thirteen Group FRA Activity Dashboard")
+        change = ChangeInfo("android_async","Android Async Programming and Services")
+        // the function above initializes the changeinfo class with five items
+    }
 }
