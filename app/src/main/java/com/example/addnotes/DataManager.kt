eb785e636bb99a96b0a706c149f786e3d70cba44
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-06-01 19:41:20 +0100

    NoteListActivity Added

--- app/src/main/java/com/example/addnotes/DataManager.kt
+++ app/src/main/java/com/example/addnotes/DataManager.kt
@@ -1,6 +1,6 @@
 package com.example.addnotes
 
-class DataManager {
+object DataManager {
     //We need to add properties to class
     //HashMap is a generic type they allow us to specify a specific type withing the type
     //HashMap accepts two type parameters
@@ -10,33 +10,56 @@ class DataManager {
     val courses = HashMap<String, CourseInfo>()
     
     //We need another property to hold our collection of changes we use array for this purpose.
-    val notes = ArrayList<CourseInfo>()
+    val notes = ArrayList<NoteInfo>()
     
     init {
         // this code will run whenever and instance of the Datamanager is created
-        intializeChanges()
+        intializeCourses()
         // initialise block accepts no parameter because there no way of running them explicitly
         // they are run automatically
+        initializeNotes()
+        
     }
     
     
     // We need to initialise our Change Info class we do this by creating a function with no parameters
 // We make this method private to the DataManager class so it's not available from anywhere else
-    private fun intializeChanges() {
+    private fun intializeCourses() {
         
         //for the HashMap lookup we do that by using it set method we pass the Id as first parameter
         // and the change var as the second parameter
         var course = CourseInfo("android_intents", "Android Programming with Intents")
         courses.set(course.courseId,course)
-        
         //Adding a few more items ot the list
         // using the Name specific Parameters the order of parameters in which they are used doesn't matter
         course = CourseInfo(title = "Java Fundamentals: The Java Language", courseId = "java_lang")
-        
+        courses.set(course.courseId,course)
         //using positional parameters
-        course = CourseInfo("report_c365","Notts Waste Audit Dashboard report")
+        course = CourseInfo("Notts_report_c365","Notts Waste Audit Dashboard report")
+        courses.set(course.courseId,course)
         course = CourseInfo("report_c365", "Thirteen Group FRA Activity Dashboard")
+        courses.set(course.courseId,course)
         course = CourseInfo("android_async", "Android Async Programming and Services")
         // the function above initializes the changeinfo class with five items
+        courses.set(course.courseId,course)
     }
+    
+    private  fun initializeNotes () {
+        var notes = NoteInfo("Android Programming with Intents","this is a text","this is a text")
+        notes = NoteInfo("Notts_report_c365","Notts Waste Audit Dashboard report","a text")
+        
+       
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- app/src/main/java/com/example/addnotes/MainActivity.kt
+++ app/src/main/java/com/example/addnotes/MainActivity.kt
@@ -7,8 +7,10 @@ import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.content_main.*
 
+@Suppress("TYPE_INFERENCE_ONLY_INPUT_TYPES_WARNING")
 class MainActivity : AppCompatActivity() {
-
+    //Property to find the position of Note
+    private var notePosition = POSITION_NOT_SET
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,17 +19,36 @@ class MainActivity : AppCompatActivity() {
         //we are now using the adaptor class to populate the spinner with the information
         // from the DataManager Class
         
-        //First we need an instance of the Datamanager Class
-        val dm = DataManager()
-        
-        val adapterCourses = ArrayAdapter(this,
-            android.R.layout.simple_spinner_item,
-            dm.courses.values.toList())
+        //Create an instance of the adapter
+        val adapterCourses = ArrayAdapter(this, // this activity as the context parameter
+            android.R.layout.simple_spinner_item, // simple_spinner_item as the layout of the spinner
+            DataManager.courses.values.toList()) //convert the courses values to a list
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        //Set another another layout to view the dropdown options
         
+        //Connect the spinner dropdown to the adapterCourses variable
         spinnerCourse.adapter = adapterCourses
+        //find the position of the note with the intent
+        // if the position is set get the value if not return -1 position
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        
+        // display the note if position is set
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
     }
-
+    
+    private fun displayNote() {
+        //find the note with it's position
+        val note = DataManager.notes[notePosition]
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+        
+        //We need to get he course information from the Spinner in DataManager
+        val coursePosition = DataManager.courses.values.indexOf<Any>(note.course)
+        spinnerCourse.setSelection(coursePosition)
+        
+    }
+    
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
