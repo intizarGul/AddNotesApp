3941eb4c92488f5ed5e77803a795418efcb55f75
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-06-27 16:17:51 +0100

    Navigation Drawer Home Fragment Implemented

--- app/src/main/java/com/example/addnotes/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/addnotes/ui/home/HomeFragment.kt
@@ -2,32 +2,39 @@
 
 package com.example.addnotes.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
-import com.example.addnotes.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.addnotes.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+	companion object {
+		fun newInstance() = HomeFragment()
+	}
 	
-	private lateinit var homeViewModel: HomeViewModel
+	private lateinit var viewModel: HomeViewModel
 	
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View? {
-		homeViewModel =
-			ViewModelProviders.of(this).get(HomeViewModel::class.java)
-		val root = inflater.inflate(R.layout.fragment_home, container, false)
-		//val textView: TextView = root.findViewById(R.id.text_home)
-		homeViewModel.text.observe(viewLifecycleOwner, Observer {
-			//textView.text = it
-		})
-		return root
+		return inflater.inflate(R.layout.fragment_home, container, false)
 	}
+	
+	override fun onActivityCreated(savedInstanceState: Bundle?) {
+		super.onActivityCreated(savedInstanceState)
+		activity.let { viewModel = ViewModelProviders.of(it!!).get(HomeViewModel::class.java) }
+		listItems.layoutManager = LinearLayoutManager(context)
+		listItems?.adapter = context?.let { NoteRecyclerAdapter(it,DataManager.notes) }
+	}
+
 }
