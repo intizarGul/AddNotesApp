ea334f51786a878fc2dd51625065e36ba8d6e128
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-07-12 16:43:43 +0100

    Project Clean up

--- app/src/main/java/com/example/addnotes/ui/courses/CoursesViewModel.kt
+++ app/src/main/java/com/example/addnotes/ui/courses/CoursesViewModel.kt
@@ -1,11 +1,6 @@
 package com.example.addnotes.ui.courses
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.addnotes.R
-import kotlinx.android.synthetic.main.content_main.*
 
 class CoursesViewModel : ViewModel() {
-var navDrawerDisplaySelection = R.id.nav_host_fragment
 }
