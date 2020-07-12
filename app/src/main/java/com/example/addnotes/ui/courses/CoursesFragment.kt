9ccbc56e3c2d294a9a9f83da299e76f7d18fadde
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-07-12 16:02:35 +0100

    Screen Rotation Stability added to the App

--- app/src/main/java/com/example/addnotes/ui/courses/CoursesFragment.kt
+++ app/src/main/java/com/example/addnotes/ui/courses/CoursesFragment.kt
@@ -1,10 +1,14 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.addnotes.ui.courses
 
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -15,15 +19,18 @@ import com.example.addnotes.DataManager
 import com.example.addnotes.NoteRecyclerAdapter
 import com.example.addnotes.R
 import com.example.addnotes.ui.home.HomeViewModel
+import kotlinx.android.synthetic.main.activity_items.*
 import kotlinx.android.synthetic.main.fragment_courses.*
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.item_course_list.*
 
 @Suppress("DEPRECATION")
-class CoursesFragment : Fragment() {
+class CoursesFragment : Fragment(R.layout.fragment_courses) {
+	
 	companion object {
 		fun newInstance () = CoursesFragment()
 	}
+	private var courseListState: Parcelable? = null
 	private val courseLayoutManager by lazy {
 		GridLayoutManager(context,2)
 	}
@@ -31,20 +38,29 @@ class CoursesFragment : Fragment() {
 		context?.let { CourseRecyclerAdapter(it,DataManager.courses.values.toList()) }
 	}
 	
-	
 	private lateinit var coursesViewModel: CoursesViewModel
 	
-	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		return inflater.inflate(R.layout.fragment_courses, container, false)
-	}
 	override fun onActivityCreated(savedInstanceState: Bundle?) {
 		super.onActivityCreated(savedInstanceState)
 		activity.let { coursesViewModel = ViewModelProviders.of(it!!).get(CoursesViewModel::class.java) }
-		listCourses.layoutManager = courseLayoutManager
-		listCourses.adapter = courseRecyclerAdapter
+		listCourses?.layoutManager = courseLayoutManager
+		listCourses?.adapter = courseRecyclerAdapter
+	}
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		courseListState = courseLayoutManager.onSaveInstanceState()
+		outState.putParcelable("LIST_STATE_KEY", courseListState)
+	}
+	
+	override fun onViewStateRestored(savedInstanceState: Bundle?) {
+		super.onViewStateRestored(savedInstanceState)
+		if (savedInstanceState != null)
+			courseListState = savedInstanceState.getParcelable("LIST_STATE_KEY")
+	}
+	
+	override fun onResume() {
+		super.onResume()
+		if (courseListState != null)
+			courseLayoutManager.onRestoreInstanceState(courseListState)
 	}
 }
