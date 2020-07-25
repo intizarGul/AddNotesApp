e54030468d05610810424c06523b726528b484fb
Intizar Gul <41545797+intizarGul@users.noreply.github.com>
2020-07-25 18:59:58 +0100

    Layout resources amended to adapt to different screen sizes

--- app/src/main/java/com/example/addnotes/ItemsActivity.kt
+++ app/src/main/java/com/example/addnotes/ItemsActivity.kt
@@ -3,12 +3,13 @@ package com.example.addnotes
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -16,12 +17,11 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_items.*
-import kotlinx.android.synthetic.main.fragment_home.listItems
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_notes.*
 
 class ItemsActivity : AppCompatActivity() {
 	private lateinit var appBarConfiguration: AppBarConfiguration
-	
 	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -35,14 +35,17 @@ class ItemsActivity : AppCompatActivity() {
 			val intentActivity = Intent(this,NoteActivity::class.java)
 			startActivity(intentActivity)
 		}
-
+		
 		val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
 		val navView: NavigationView = findViewById(R.id.nav_view)
-		val navController = findNavController(R.id.nav_host_fragment)
+		val navHostFragment =
+			supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+		val navController: NavController = navHostFragment.navController
 		// Passing each menu ID as a set of Ids because each
 		// menu should be considered as top level destinations.
 		appBarConfiguration = AppBarConfiguration(setOf(
-			R.id.nav_home, R.id.nav_notes, R.id.nav_courses), drawerLayout)
+			
+			R.id.nav_home, R.id.nav_notes, R.id.nav_courses,R.id.nav_how_many), drawerLayout)
 		setupActionBarWithNavController(navController, appBarConfiguration)
 		navView.setupWithNavController(navController)
 	}
